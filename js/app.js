82060d5c6c984ed61ee94568df62c54b88143570
nancyzj <ancizj@hotmail.com>
2017-06-05 15:44:35 -0500

    creacion de mapa y maquetacion

--- js/app.js
+++ js/app.js
@@ -1,43 +1,32 @@
-// function initMap() {
-//     map = new google.maps.Map(document.getElementById('mapa'), {
-//       center: {lat:19.4211655, lng:-99.165415},
-//       zoom: 
-// });
-// }
 var cargarPagina = function () {
-	// $("#get-location").click(obtenerUbicacion);
-	mostrarPosicion();
-
+	$("#obtener-localizacion").click(obtenerUbicacion);
 };
 
-// var obtenerUbicacion = function (e) {
-// 	if (navigator.geolocation) {
-// 		navigator.geolocation.getCurrentPosition(mostrarPosicion);
-// 	} else {
-// 		alert("Actualice su navegador");
-// 	}
-// };
+var obtenerUbicacion = function (e) {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(mostrarPosicion);
+	} else {
+		alert("Actualice su navegador");
+	}
+};
 
 var mostrarPosicion = function (posicion) {
 	console.log(posicion);
-	// alert("Latitud: " + posicion.coords.latitude);
-	// alert("Longitud: " + posicion.coords.longitude);
-
 	var coordenadas = {
-		lat:19.4211655, 
-		lng:-99.165415
+		lat: posicion.coords.latitude, 
+		lng: posicion.coords.longitude
 	};
 	mostrarMapa(coordenadas);
 };
 
 var mostrarMapa = function (coordenadas) {
-	var mapa = new google.maps.Map($('#mapa')[0], {
+	var map = new google.maps.Map($('#mapa')[0], {
       zoom: 17,
       center: coordenadas
     });
     var marker = new google.maps.Marker({
       position: coordenadas,
-      mapa: mapa
+      map: map
     });
 }
 
